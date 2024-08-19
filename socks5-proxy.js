e1aea8f5d37c8a8354c8c649725c2c8bbfd943e6
Bluezly <141844841+Bluezly@users.noreply.github.com>
2024-08-19 08:55:22 +0300

    Update socks5-proxy.js

--- socks5-proxy.js
+++ socks5-proxy.js
@@ -1,21 +1,16 @@
+const SocksProxyAgent = require('socks5-http-client/lib/Agent');
 const http = require('http');
-const httpProxy = require('http-proxy');
 
-// إنشاء بروكسي
-const proxy = httpProxy.createProxyServer({});
+// إعداد البروكسي SOCKS5
+const proxyAgent = new SocksProxyAgent('socks5://localhost:1080'); // تأكد من أن هذا هو عنوان البروكسي الصحيح
 
 // إنشاء خادم HTTP
 const server = http.createServer((req, res) => {
-  // استخدم البروكسي لتوجيه الطلبات إلى الوجهة الأصلية المطلوبة من قبل العميل
-  proxy.web(req, res, { target: req.url }, (err) => {
-    console.error(err);
-    res.writeHead(500, { 'Content-Type': 'text/plain' });
-    res.end('Proxy Error');
-  });
+  // استخدم البروكسي لتوجيه الطلبات
+  req.pipe(proxyAgent.request(req.url)).pipe(res);
 });
 
-// استماع الخادم على منفذ 8080
-const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
-  console.log(`Proxy server is running on port ${PORT}`);
+// استماع الخادم على منفذ 10000
+server.listen(10000, () => {
+  console.log('Proxy server is running on port 10000');
 });
